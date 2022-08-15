680c42873ffd8b35e217008152fd8295dc9b0e8c
Archie Fan <archiefan@gmail.com>
2022-08-15 20:41:17 +0100

    Add project files.

---
+++ CachingApp/Pages/FetchData.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using DataAccessLibrary;
+
+namespace CachingApp.Pages
+{
+    public partial class FetchData
+    {
+        [Inject]
+        private EmployeesDataAccess? EmployeesService { get; set; }
+
+        List<Employee>? employees;
+
+        protected override async Task OnInitializedAsync()
+        {
+            //employees = EmployeesService.GetEmployees();
+            //employees = await EmployeesService.GetEmployeesAsync();
+            employees = await EmployeesService.GetEmployeesCache();
+        }
+    }
+}
---
+++ DataAccessLibrary/EmployeesDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class EmployeesDataAccess
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public EmployeesDataAccess(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new();
+            employees.Add(new Employee { Id = 1, FirstName = "FN1", LastName = "LN1" });
+            employees.Add(new Employee { Id = 2, FirstName = "FN2", LastName = "LN2" });
+            employees.Add(new Employee { Id = 3, FirstName = "FN3", LastName = "LN3" });
+            Thread.Sleep(3000);
+            return employees;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            List<Employee> employees = new();
+            employees.Add(new Employee { Id = 1, FirstName = "FN1", LastName = "LN1" });
+            employees.Add(new Employee { Id = 2, FirstName = "FN2", LastName = "LN2" });
+            employees.Add(new Employee { Id = 3, FirstName = "FN3", LastName = "LN3" });
+            await Task.Delay(3000);
+            return employees;
+        }
+
+        public async Task<List<Employee>> GetEmployeesCache()
+        {
+            List<Employee> employees;
+            employees = _memoryCache.Get<List<Employee>>("employees");
+            if (employees is null)
+            {
+                employees =  new();
+                employees.Add(new Employee { Id = 1, FirstName = "FN1", LastName = "LN1" });
+                employees.Add(new Employee { Id = 2, FirstName = "FN2", LastName = "LN2" });
+                employees.Add(new Employee { Id = 3, FirstName = "FN3", LastName = "LN3" });
+                await Task.Delay(3000);
+                _memoryCache.Set("employees", employees, TimeSpan.FromMinutes(1));
+            }
+            return employees;
+        }
+    }
+}
